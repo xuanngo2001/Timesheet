268262acd4d110a2e9688849e56d7f7951eecbfd
limelime <limelime@users.noreply.github.com>
2018-09-13 20:10:23 +0000

    pack to release/.

--- pack.sh
+++ pack.sh
@@ -1,2 +1,5 @@
 date_string=$(date +"%Y-%m-%d.%0k.%M.%S")
-tar -czvf timesheet_${date_string}.tar.gz timesheet
+output_file="release/timesheet_${date_string}.tar.gz"
+tar -czvf "${output_file}" timesheet > /dev/null
+
+echo "${output_file} is created."
