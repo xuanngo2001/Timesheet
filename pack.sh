724746a3fb3f5dd5c23027685533f5b9135b8cf0
limelime <limelime@users.noreply.github.com>
2018-09-13 20:23:58 +0000

    ensure release dir is created.

--- pack.sh
+++ pack.sh
@@ -1,5 +1,7 @@
 date_string=$(date +"%Y-%m-%d.%0k.%M.%S")
-output_file="release/timesheet_${date_string}.tar.gz"
-tar -czvf "${output_file}" timesheet > /dev/null
+base_dir="release"
+	mkdir -p "${base_dir}"
+output_file="${base_dir}/timesheet_${date_string}.tar.gz"
 
+tar -czvf "${output_file}" timesheet > /dev/null
 echo "${output_file} is created."
